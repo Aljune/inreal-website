1b5eda3c56d190868f6667733be13601e6073f4e
degamAljune <degamoaljune14@gmail.com>
2025-09-04 00:18:10 +0800

    update app inreal apk website reove

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
-import App from "./App";
 import "./index.css";
 import Index from "./pages/Index";
 
